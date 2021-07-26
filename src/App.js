de48e5860759a35c04e1745117bf6b02da42cbb8
rabbit-22 <chlwlal20@gmail.com>
2021-07-26 17:58:43 +0900

    State 사용

--- src/App.js
+++ src/App.js
@@ -7,11 +7,22 @@ import Subject from './components/Subject';
 import MainWrap from './components/MainWrap';
 
 class App extends Component{
+  constructor(props){
+    super(props); // 어떤 컴포넌트가 실행될때 render()보다 먼저 실행되면서 초기화를 담당하는 코드를 cunstructor 안에 작성
+    this.state = { // 상위 컴포넌트의 state 값을 하위 컴포넌트의 props값으로 전달하는 것은 얼마든지 가능하다
+      subject:{title:'WEB', sub:'World WIde Web!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is  for information ...'},
+        {id:2, title:'CSS', desc:'CSS is  for design ...'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive ...'},
+      ]
+    }
+  }
   render(){
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web"></Subject>
-        <TOC></TOC>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="우왕.."></Content>
         <MainWrap></MainWrap>
       </div>
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -3,12 +3,18 @@ import React, { Component } from 'react';
 // react라는 라이브러리에서 component라는 클래스를 로딩
 class TOC extends Component{
     render(){
+      var lists = [];
+      var data = this.props.data;
+      var i = 0;
+      while(i<data.length){
+        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
+        // key 값이 필요함
+        i = i + 1;
+      }
       return (
         <nav>
               <ul>
-                  <li><a href="1.html">HTML</a></li>
-                  <li><a href="2.html">CSS</a></li>
-                  <li><a href="3.html">JavaScript</a></li>
+                  {lists}
               </ul>
           </nav>
       );
